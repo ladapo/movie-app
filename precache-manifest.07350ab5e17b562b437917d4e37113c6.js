6af17e3f33c22d10953203a0fcfd0d5bd53b5b4f
Akinola Oladapo <akinola_oladapo@yahoo.com>
2020-05-30 20:22:24 +0100

    Updates

---
+++ precache-manifest.07350ab5e17b562b437917d4e37113c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c374cfe02e0158fe89567b47d5e0ea83",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "b8560f73ace38d064186",
+    "url": "/movie-app/static/css/main.ae9f7292.chunk.css"
+  },
+  {
+    "revision": "f1c8e7ac44821add36f3",
+    "url": "/movie-app/static/js/2.88aa7bd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app/static/js/2.88aa7bd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8560f73ace38d064186",
+    "url": "/movie-app/static/js/main.8ee7974c.chunk.js"
+  },
+  {
+    "revision": "ef13b2d46e7e451c8c9d",
+    "url": "/movie-app/static/js/runtime-main.4744ba8c.js"
+  }
+]);
